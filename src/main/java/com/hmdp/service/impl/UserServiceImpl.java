81fdc9bba8b2deb7cb47ccba9f3817e6c7ab5b31
laiiiii <laiiiii@foxmail.com>
2026-03-09 04:15:52 +0800

    短信登录-实现发送短信验证码功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,13 +1,45 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import cn.hutool.core.util.RandomUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送手机短信验证码
+     * @param phone 手机号码，用于接收短信验证码
+     * @param session HTTP 会话对象，用于保存生成的验证码供后续验证使用
+     * @return 操作结果，手机号格式正确返回成功，否则返回"手机号格式错误!"
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1. 校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误!");
+        }
+
+        // 3.符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 4.保存验证码到session
+        session.setAttribute("code", code);
+
+        // 5.发送验证码
+        log.debug("发送短信验证码成功，验证码：{}", code);
+
+        // 6.返回结果
+        return Result.ok();
+    }
 }
