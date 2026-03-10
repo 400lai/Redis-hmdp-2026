63be2d6f77771e870fae4607d3932a542672ab1f
laiiiii <laiiiii@foxmail.com>
2026-03-10 20:43:07 +0800

    优惠券秒杀-redis实现全局唯一id

--- src/main/java/com/hmdp/entity/Voucher.java
+++ src/main/java/com/hmdp/entity/Voucher.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -78,12 +79,14 @@ public class Voucher implements Serializable {
      * 生效时间（非数据库字段，用于秒杀券）
      */
     @TableField(exist = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime beginTime;
 
     /**
      * 失效时间（非数据库字段，用于秒杀券）
      */
     @TableField(exist = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endTime;
 
     /**
