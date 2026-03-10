724f01504115bc04042edfe54515bcc149ad3832
laiiiii <laiiiii@foxmail.com>
2026-03-11 00:47:59 +0800

    优惠券秒杀-乐观锁解决超卖

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -51,10 +51,12 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足！");
         }
 
-        // 5.扣减库存
+        // 5.扣减库存，乐观锁防止线程冲突
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)    // where id = ? and stock > 0
+                .update();
 
         if (!success) {
             // 扣减库存失败
