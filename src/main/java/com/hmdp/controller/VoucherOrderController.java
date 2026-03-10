ba84c0a7262b183ba408a2d264ad25a96ef04225
laiiiii <laiiiii@foxmail.com>
2026-03-10 21:04:33 +0800

    优惠券秒杀-实现秒杀下单

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -2,10 +2,13 @@ package com.hmdp.controller;
 
 
 import com.hmdp.dto.Result;
+import com.hmdp.service.IVoucherOrderService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * 优惠券订单控制器 - 处理秒杀下单请求
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/voucher-order")
 public class VoucherOrderController {
 
+    @Resource
+    private IVoucherOrderService voucherOrderService;
+
     /**
      * 秒杀优惠券下单接口
      * @param voucherId 优惠券 ID
@@ -21,6 +27,6 @@ public class VoucherOrderController {
      */
     @PostMapping("seckill/{id}")
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
-        return Result.fail("功能未完成");
+        return voucherOrderService.seckillVoucher(voucherId);
     }
 }
